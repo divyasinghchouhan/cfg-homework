eb2d92723a81aa1d7666d5c9d7d1c8452b0606a3
divyasinghchouhan <divyachouhanuk@gmail.com>
2022-10-16 16:37:18 +0100

    Add finished homework

--- homework-1/cash-register.py
+++ homework-1/cash-register.py
@@ -19,29 +19,70 @@ class CashRegister:
 
     def __init__(self):
 
-        self.total_items = None # {'item': 'price'}
+        self.total_items = {} # {'item': 'price'}
         self.total_price = 0
         self.discount = 0
 
-    def add_item(self):
-        pass
+    def add_item(self, name, price):
+        self.total_items[name.capitalize()] = price
+        self.total_price += price
 
-    def remove_item(self):
-        pass
+        print('{:<20} {:<10.2f}'.format(name.capitalize(), price))
 
-    def apply_discount(self):
-        pass
+    def remove_item(self, name):
+        try:
+            removed = self.total_items.pop(name.capitalize())
+        except KeyError:
+            print("Item not found.")
+        else:
+            self.total_price -= removed
+            item_cancelled = f"*CANCELLED* {name.capitalize()}"
+            price_item_cancelled = f"–{removed:.2f}"
+            print('{:<18} {:<10}'.format(item_cancelled, price_item_cancelled))
+
+    def apply_discount(self, discount_percentage):
+        self.discount = self.total_price * discount_percentage / 100
 
     def get_total(self):
-        pass
+        print('-------------------------------------------')
+
+        # printing the sub total, discount and final total
+        print('{:<20} £{:<10.2f}'.format('Total Price', self.total_price))
+        print('{:<20} £{:<10.2f}'.format('Discount', self.discount))
+        print('-------------------------------------------')
+        print('{:<20} £{:<10.2f}'.format('Final Price :', self.total_price - self.discount))
 
     def show_items(self):
-        pass
+        for item in self.total_items:
+            print('{:<20} {:<10.2f}'.format(item, self.total_items.get(item)))
+
+        print('-------------------------------------------')
+        # printing the sub total, discount and final total, properly formatted
+        print('{:<20} £{:<10.2f}'.format('Total', self.total_price))
+        print('{:<20} £{:<10.2f}'.format('Discount', self.discount))
+        print('-------------------------------------------')
+        print('{:<20} £{:<10.2f}'.format('Final', self.get_total()))
 
     def reset_register(self):
-        pass
+        self.total_items = {}
+        self.total_price = 0
+        self.discount = 0
 
 
 # EXAMPLE code run:
 
 # ADD
+if __name__ == '__main__':
+    # creating a new register
+    my_register1 = CashRegister()
+    # adding some items to the register
+    print('{:<20} {:<10}'.format('Item', 'Price'))
+    my_register1.add_item('Milk', 1.00)
+    my_register1.add_item('Bread', 1.50)
+    my_register1.add_item('Apples', 1.00)
+    my_register1.add_item('Bananas', 1.00)
+    # removing 'Bananas' from the register, and updating the price
+    my_register1.remove_item('Bananas')
+    # applying a 15% discount to total price
+    my_register1.apply_discount(15)
+    my_register1.get_total()
